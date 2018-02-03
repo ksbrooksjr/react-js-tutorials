9d8ca684fb5039b12ab48b5a6578cc5639d760e1
Keith Brooks <keith1225@outlook.com>
2018-02-03 13:54:58 -0500

    Moves actions to MobX, uses strict mode, converts component into a SFC

--- 6-mobx-react/src/js/TodoList.js
+++ 6-mobx-react/src/js/TodoList.js
@@ -2,38 +2,51 @@ import React from "react"
 import { observer } from "mobx-react"
 
 
-@observer
-export default class TodoList extends React.Component {
-  createNew(e) {
-    if (e.which === 13) {
-      this.props.store.createTodo(e.target.value)
-      e.target.value = ""
-    }
+const todoList = observer(({store}) => {
+  const {
+    clearComplete,
+    filter,
+    filteredTodos,
+    todos,
+    filterMethod,
+    toggleComplete,
+    createNew
+  } = store
+
+  const todoList = filteredTodos.map(todo => (
+    <li key={todo.id}>
+     <input type="checkbox" onChange={() => toggleComplete(todo)} value={todo.complete} checked={todo.complete} />
+     <span>{todo.value}</span>
+    </li>
+  ))
+
+  const styles = {
+    display: "block",
+    marginTop: 20,
+    marginBottom: 10
   }
 
-  filter(e) {
-    this.props.store.filter = e.target.value
-  }
-
-  toggleComplete(todo) {
-    todo.complete = !todo.complete
-  }
-
-  render() {
-    const { clearComplete, filter, filteredTodos, todos } = this.props.store
-
-    const todoLis = filteredTodos.map(todo => (
-      <li key={todo.id}>
-       <input type="checkbox" onChange={this.toggleComplete.bind(this, todo)} value={todo.complete} checked={todo.complete} />
-       <span>{todo.value}</span>
-      </li>
-    ))
-    return <div>
+  return(
+    <div>
       <h1>todos</h1>
-      <input className="new" onKeyPress={this.createNew.bind(this)} />
-      <input className="filter" value={filter} onChange={this.filter.bind(this)} />
-      <ul>{todoLis}</ul>
+
+      <label style={styles}>New Todo</label>
+      <input
+        className="new"
+        onKeyPress={createNew}
+        />
+
+      <label style={styles}>Filter Todos</label>
+      <input
+        className="filter"
+        value={filter}
+        onChange={filterMethod}
+        />
+
+      <ul>{todoList}</ul>
       <a href="#" onClick={clearComplete}>Clear Complete</a>
     </div>
-  }
-}
+  )
+})
+
+export default todoList;
--- 6-mobx-react/src/js/TodoStore.js
+++ 6-mobx-react/src/js/TodoStore.js
@@ -1,4 +1,6 @@
-import { computed, observable } from "mobx"
+import { action, computed, observable, useStrict } from "mobx"
+
+useStrict(true)
 
 class Todo {
   @observable value
@@ -15,20 +17,36 @@ class Todo {
 export class TodoStore {
   @observable todos = []
   @observable filter = ""
+
   @computed get filteredTodos() {
     var matchesFilter = new RegExp(this.filter, "i")
     return this.todos.filter(todo => !this.filter || matchesFilter.test(todo.value))
   }
 
-  createTodo(value) {
+  @action createTodo(value) {
     this.todos.push(new Todo(value))
   }
 
-  clearComplete = () => {
+  @action clearComplete = () => {
     const incompleteTodos = this.todos.filter(todo => !todo.complete)
     this.todos.replace(incompleteTodos)
   }
+
+  @action filterMethod = (e) => {
+    this.filter = e.target.value;
+  }
+
+  @action toggleComplete = (todo) => {
+    todo.complete = !todo.complete
+  }
+
+  createNew = (e) => {
+    if (e.which === 13) {
+      this.createTodo(e.target.value)
+      e.target.value = ""
+    }
+  }
+
 }
 
 export default new TodoStore
-
